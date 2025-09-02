00449c43047604804e662d9f7aed3d4f6033eb6d
HarveySears9 <c1005127@newcastle.ac.uk>
2025-09-02 17:43:11 +0100

    Combined Level 1 and Level 2 Scenes
    - Makes it easier to add features to all levels
    - Makes it easer to add more levels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject[] characters;
-    public GameObject Spawner;
-    public GameObject tileMapPainter;
+    public GameObject[] levels;
     public DeathScreen deathScreen;
+
+    public int currentLevel = 1;
     
     void Start()
     {
         SetCharacter();
-        Spawner.SetActive(true);
-        tileMapPainter.SetActive(true);
+        SetLevel(currentLevel);
     }
 
     private void SetCharacter()
@@ -21,5 +21,21 @@ public class GameManager : MonoBehaviour
         characters[SaveFile.LoadData<SaveFile.Data>().currentCharacter].SetActive(true); 
         characters[SaveFile.LoadData<SaveFile.Data>().currentCharacter].tag = "Player";
         deathScreen.pc = characters[SaveFile.LoadData<SaveFile.Data>().currentCharacter].GetComponent<PlayerController>();
+    }
+
+    private void SetLevel(int level)
+    {
+        switch (level)
+        {
+            case 1:
+                levels[0].SetActive(true);
+                break;
+            case 2:
+                levels[1].SetActive(true);
+                break;
+            default:
+                levels[0].SetActive(true);
+                break;
+        }
     }
 }
