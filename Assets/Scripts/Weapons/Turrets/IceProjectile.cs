c51a064d73ec522e793d55933d692141e6754a91
HarveySears9 <c1005127@newcastle.ac.uk>
2026-09-11 11:52:31 +0100

    Fixed Bug that causes Penguin AOE to crash game

--- Assets/Scripts/Weapons/Turrets/IceProjectile.cs
+++ Assets/Scripts/Weapons/Turrets/IceProjectile.cs
@@ -9,22 +9,23 @@ public class IceProjectile : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy"))
-        {
-            EnemyController enemy = collision.GetComponent<EnemyController>();
-            Boss boss = collision.GetComponent<Boss>();
-            if (enemy != null)
-            {
-                enemy.ApplySlow(0.4f, 2f); // 40% slow for 2 seconds
-            }
-            else if(boss != null)
-            {
-                boss.ApplySlow(0.4f, 2f);
-            }
+        if (!collision.CompareTag("Enemy"))
+            return;
 
-            if (destroyOnHit)
-                Destroy(gameObject); // Or pool it if you use pooling
+        EnemyController enemy = collision.GetComponent<EnemyController>();
+        Boss boss = collision.GetComponent<Boss>();
+
+        if (enemy != null)
+        {
+            enemy.ApplySlow(0.4f, 2f);
         }
+        else if (boss != null)
+        {
+            boss.ApplySlow(0.4f, 2f);
+        }
+
+        if (destroyOnHit)
+            Destroy(gameObject);
     }
 
 }
